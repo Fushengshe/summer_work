aaac2c7fb1df60968bae0c86a17a4b2fd57b269a
Raoul1996 <baobaozhou199606@gmail.com>
2016-07-16 19:34:58 +0800

    补充图片

---
+++ work/demo/day-004/index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Raoul on 2016/7/16.
+ */
+window.onload =function () {
+    var title =document.getElementById("title");
+    var time =document.getElementById("time");
+    var content =document.getElementById("content");
+    var oBun = document.getElementsByTagName("button");
+    var oUl =document.getElementsByTagName("ul")[0];
+    var oli =document.getElementsByName("li");
+    oBun[0].onclick =function () {
+        if(time.value==""||title.value==""||content.value==""){
+            alert("请填写完整！")
+        }else{
+            var oLi =document.createElement("li");
+            var string =time.value +"--"+ content.value;
+            oLi.innerHTML=string;
+            oUl.appendChild(oLi);
+        }
+    }
+    oBun[1].onclick =function () {
+        time.value="";
+        title.value="";
+        content.value="";
+        oli.innerHTML="";
+    }
+
+}
