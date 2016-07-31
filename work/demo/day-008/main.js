9af1ed95eb6707bc53b02cd1911f6ec0612414da
Raoul1996 <baobaozhou199606@gmail.com>
2016-07-31 11:33:57 +0800

    day008 移除localstorage

--- work/demo/day-008/main.js
+++ work/demo/day-008/main.js
@@ -1,5 +1,7 @@
 /**
  * Created by Raoul on 2016/7/16.
+ * copy form day-007.
+ * delete localstorange.
  */
     var title =document.getElementById("title");
     var time =document.getElementById("time");
@@ -9,6 +11,7 @@
     var oInput =document.getElementsByTagName("input");
     // var oLi = document.getElementsByTagName("li");
     var ii =localStorage.getItem("num");
+    console.dir(localStorage);
         function clearColor() {
         for (var n =0; n<oInput.length;n++){
             // oInput[n].style.backgroundColor="";
@@ -24,61 +27,46 @@
                 if(oInput[n].value==""){
                     oInput[n].style.backgroundColor="red";
                 }
+
             }
             alert("请填写完整！");
         }else{
             ii++;
             var oLi =document.createElement("li");
-            localStorage.setItem("title"+[ii],title.value);
-            localStorage.setItem("time"+[ii],time.value);
-            localStorage.setItem("content"+[ii],content.value);
-            localStorage.setItem("num",ii);
-            console.dir(localStorage);
-            console.dir(ii);
+            // localStorage.setItem("title"+[ii],title.value);
+            // localStorage.setItem("time"+[ii],time.value);
+            // localStorage.setItem("content"+[ii],content.value);
+            // localStorage.setItem("num",ii);
+            // console.dir(localStorage);
+            // console.dir(ii);
             var oSpan = document.createElement("span");
             oLi.appendChild(oSpan);
-            oSpan.innerHTML=localStorage.getItem("title"+[ii]);
+            // oSpan.innerHTML=localStorage.getItem("title"+[ii]);
             var oSpan1 = document.createElement("span");
             oLi.appendChild(oSpan1);
-            oSpan1.innerHTML=localStorage.getItem("time"+[ii]);
-            //将input中的字符串设置为li的属性，并显示
+            // oSpan1.innerHTML=localStorage.getItem("time"+[ii]);
+            oLi.setAttribute("content",content.value);
+            oLi.setAttribute("title",title.value);
+            oLi.setAttribute("time",time.value);
+            oSpan1.innerHTML = oLi.getAttribute("time");
+            oSpan.innerHTML = oLi.getAttribute("title");
+            //将input中的字符串设置为li的属性，并显示.
             oLi.addEventListener('click',function () {
-                oInput[0].value=localStorage.getItem("title"+[ii]);
-                oInput[1].value=localStorage.getItem("time"+[ii]);
-                oInput[2].value =localStorage.getItem("content"+[ii]);
-
+                oInput[0].value=oLi.getAttribute("title");
+                oInput[1].value=oLi.getAttribute("time");
+                oInput[2].value =oLi.getAttribute("content");
                 clearColor();
             },false);
             oUl.appendChild(oLi);
-            // clearColor();
+            clearColor();
         }
     };
-    function set(i) {
-    var oLi =document.createElement("li");
-    var oSpan = document.createElement("span");
-    oLi.appendChild(oSpan);
-    oSpan.innerHTML=localStorage.getItem("title"+[i]);
-    var oSpan1 = document.createElement("span");
-    oLi.appendChild(oSpan1);
-    oSpan1.innerHTML=localStorage.getItem("time"+[i]);
-    //将input中的字符串设置为li的属性，并显示
-    oLi.addEventListener('click',function () {
-        oInput[0].value=localStorage.getItem("title"+[i]);
-        oInput[1].value=localStorage.getItem("time"+[i]);
-        oInput[2].value =localStorage.getItem("content"+[i]);
-        clearColor();
-    },false);
-    oUl.appendChild(oLi);
-    clearColor();
-};
-
     oBun[1].onclick =function () {
         time.value="";
-
         title.value="";
         content.value="";
         localStorage.clear();
-        console.log(localStorage);
+        // console.log(localStorage);
         console.log(oLi.length);
             if(oUl.firstElementChild===oUl.lastElementChild){
                 alert("不可以清除了");
@@ -89,7 +77,26 @@
             }
         clearColor();
     };
-    for(var i = 1; i<= localStorage.getItem("num");i++){
-        set(i);
-};//实现使用localstorage存储数据。
+//     for(var i = 1; i<= ii;i++){
+//     // var oLi = document.createElement("li");
+//     var oLi =document.createElement("li");
+//     var oSpan = document.createElement("span");
+//     oLi.appendChild(oSpan);
+//     oSpan.innerHTML=localStorage.getItem("title"+[i]);
+//     var oSpan1 = document.createElement("span");
+//     oLi.appendChild(oSpan1);
+//     oSpan1.innerHTML=localStorage.getItem("time"+[i]);
+//         oLi.setAttribute("content",localStorage.getItem("content"+[i]));
+//         oLi.setAttribute("title",localStorage.getItem("title"+[i]));
+//         oLi.setAttribute("time",localStorage.getItem("time"+[i]));
+//     //将input中的字符串设置为li的属性，并显示
+//     oLi.addEventListener('click',function () {
+//         oInput[0].value=this.getAttribute("title");
+//         oInput[1].value=this.getAttribute("time");
+//         oInput[2].value =this.getAttribute("content");
+//         // clearColor();
+//     },false);
+//     oUl.appendChild(oLi);
+//     // clearColor();
+// };//实现使用localstorage存储数据。
 
